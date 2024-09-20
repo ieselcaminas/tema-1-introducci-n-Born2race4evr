37df5ffbf2d8dea4d9796e5dc12b2514f023d63f
julpermaz <julpermaz@alu.edu.gva.es>
2024-09-20 11:45:44 +0200

    Ej22

---
+++ src/main/Ej22.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ej22 {
+    public static void main(String[] args) {
+        int num;
+        int positivo = 0;
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i <= 10; i++) {
+            num = sc.nextInt();
+            if (num > 0) {
+                positivo ++;
+            }
+        }
+        System.out.println("Positivos: " + positivo);
+
+    }
+}
